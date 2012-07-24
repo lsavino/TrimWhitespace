2c0b8a8c4c4c60b1d5aac102168f3f8848fa63b4
Laura Savino <lsavino@ubermind.com>
2012-07-23 21:36:17 -0700

    Trim trailing whitespace and reverse all the letters

--- main.hs
+++ main.hs
@@ -1,10 +1,20 @@
 module Main where
 
 import System.IO
+import Data.Char
 
 main :: IO ()
 main = do
     inFD <- openFile "sample_input.txt" ReadMode
     inString <- hGetContents inFD
     outFD <- openFile "sample_output.txt" WriteMode
-    hPutStr outFD inString
+    let fileLines = lines inString
+        trimmedFileLines = map trimLine fileLines
+    mapM putStrLn trimmedFileLines
+    return ()
+
+trimLine :: String -> String
+trimLine str = 
+    let reversed = reverse str
+    in dropWhile isSpace reversed
+
