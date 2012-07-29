9445c04cb77170057e87e24697a1d7883a6aba28
Laura Savino <lsavino@ubermind.com>
2012-07-29 16:31:09 -0700

    Passed an argument from the command line

--- main.hs
+++ main.hs
@@ -3,18 +3,21 @@ module Main where
 import Data.Char
 import System.Directory
 import System.IO
+import System.Environment
 
 main :: IO ()
 main = do
-    inFD <- openFile "sample_input.txt" ReadMode
+    args <- getArgs
+    let fileName = head args
+    inFD <- openFile fileName ReadMode
     inString <- hGetContents inFD
     outFD <- openFile "sample_output.txt" WriteMode
 
     let trimmedFileLines = map trimLine $ lines inString
     mapM (hPutStrLn outFD) trimmedFileLines
 
-    removeFile "sample_input.txt"
-    renameFile "sample_output.txt" "sample_input.txt"
+    removeFile fileName
+    renameFile "sample_output.txt" fileName
     hClose outFD
     hClose inFD
 
